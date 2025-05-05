fde8033a02f001affffda68dcaff1c673b8f7487
otake <ootake1@gmail.com>
2025-05-05 20:44:46 +0900

    update: package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SymbolExtension",
+    name: "symbol-extension",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
